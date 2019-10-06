eb71a6c99438a7fe714e49ad0121c86a2b98eaca
dilipRathoreRepo <rathore.gecg@gmail.com>
2019-10-06 16:17:29 +0530

    modifyingVariablesFile

---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "project" {
+	type = string
+	default = "rising-minutia-254502"
+}
+variable "region" {
+	type = string
+	default = "europe-west1"
+}
+variable "credentials" {
+	type = string
+	default = "${file("/Users/diliprathore/terraform/terraform-working/creds/serviceaccount.json")}"
+}
+variable "cluster_name" {
+	type = string
+	default = "mycluster"
+}
+variable "network" {
+	type = string
+	default = "default"
+}
+variable "location" {
+	type = string
+	default = "europe-west1"
+}
+variable "remove_default_node_pool" {
+	type = bool
+	default = true
+}
+variable "initial_node_count" {
+	type = number
+	default = 1
+}
+variable "node_pool_name" {
+	type = string
+	default = "my-node-pool"
+}
+variable "preemptible" {
+	type = bool
+	default = false
+}
+variable "machine_type" {
+	type = string
+	default = "n1-standard-1"
+}
+variable "oauth_scopes" {
+	type = list(string)
+	default = [
+	  "https://www.googleapis.com/auth/logging.write",
+	  "https://www.googleapis.com/auth/monitoring",
+	  "https://www.googleapis.com/auth/pubsub",
+	  "https://www.googleapis.com/auth/bigquery"
+]
+}
